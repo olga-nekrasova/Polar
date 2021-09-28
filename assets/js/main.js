a0a925acb1e1beb2dfc9085b3da18aac3b5edae4
olga-nekrasova <86124453+olga-nekrasova@users.noreply.github.com>
2021-09-28 21:49:01 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".next_slide",
+        prevEl: ".prev_slide",
+    },
+    thumbs: {
+        swiper: swiper,
+    },
+});
